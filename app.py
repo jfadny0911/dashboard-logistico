52942ace79863c651209666f619142e7430d8085
jfadny0911 <116517266+jfadny0911@users.noreply.github.com>
2025-11-22 13:04:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} i
 
 # CLAVE GEMINI (INTEGRADA DIRECTAMENTE)
 # ADVERTENCIA: DEBES REEMPLAZAR ESTA CLAVE POR UNA NUEVA Y VÁLIDA
-GEMINI_API_KEY = "AIzaSyB4Pl0C99b5zOEvplcoBgGzS4VnmLMLIi8" 
+GEMINI_API_KEY = "AIzaSyDgOVmirsUOkcbocawuAIbs0jjLiWqM5Ww" 
 
 # Inicialización del Cliente Gemini
 client = None
